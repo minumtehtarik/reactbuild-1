2b12f73a96b7e8b4554e92e00f45c6e960fc6cd4
Tri Abadi <triabadi09@gmail.com>
2021-08-05 21:33:10 +0700

    Perbaikan css code

--- src/component/Expenses/ExpensesItem.js
+++ src/component/Expenses/ExpensesItem.js
@@ -6,7 +6,7 @@ function ExpensesItem(props) {
     return (
         <div className="expense-item">
             <div>{props.date.toISOString()}</div>
-            <div  className="expense-item-description">
+            <div  className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
