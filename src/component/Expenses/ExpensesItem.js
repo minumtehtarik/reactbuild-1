bda83721ad231bf65612ac81ed90c87436e8e1dc
Tri Abadi <triabadi09@gmail.com>
2021-08-05 07:54:04 +0700

    adding JSX

---
+++ src/component/Expenses/ExpensesItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function ExpensesItem() {
+
+    return (
+        <div>
+            <h2>Expenses Item</h2>
+            <div>
+                <h2>Andi</h2>
+                <div>$100.30</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpensesItem;
