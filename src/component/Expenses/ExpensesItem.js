3451764674245da8a456ded3cf1fae07d5555ffc
Tri Abadi <triabadi09@gmail.com>
2021-08-11 18:22:45 +0700

    Concept of Composition

--- src/component/Expenses/ExpensesItem.js
+++ src/component/Expenses/ExpensesItem.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import Card from './Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpensesItem.css'
 
 function ExpensesItem(props) {
 
     return (
-        <div className="expense-item">
+        <Card className="expense-item">
             <ExpenseDate date={props.date}/>
             <div  className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
-        </div>
-    );
+        </Card>
+     );
+
+      
 }
 
 export default ExpensesItem;
